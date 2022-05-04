1238af7fbe77892f890bd57efeb6fb2dde2567fb
Shubham Patrick <ahthneeuhl@gmail.com>
2022-05-04 16:01:29 +0530

    Styling the Input Element | Configuring the TextInput Field | Adding Visual Feedback to the Buttons | Improving the Buttons

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,11 +1,49 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const CustomButton = ({ children }) => {
+  const pressHandler = () => {
+    console.log("Pressed");
+  };
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        android_ripple={styles.ripple}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default CustomButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#801F50",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    textAlign: "center",
+  },
+  ripple: {
+    color: "#8E2F5C",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,20 @@ import CustomButton from "../components/CustomButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
-      <CustomButton>Reset</CustomButton>
-      <CustomButton>Confirm</CustomButton>
+      <TextInput
+        style={styles.styledInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <CustomButton>Reset</CustomButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <CustomButton>Confirm</CustomButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -15,6 +26,7 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
@@ -25,5 +37,22 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  styledInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#DDB52F",
+    borderBottomWidth: 2,
+    color: "#DDB53F",
+    marginVertical: 8,
+    fontWeight: "bold",
+    width: 80,
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
