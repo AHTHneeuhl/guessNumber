ae26ebe2c57dd3bc756842e1cf858b34210b3d03
Shubham Patrick <ahthneeuhl@gmail.com>
2022-05-06 23:05:35 +0530

    Adding a (foreground) Image | Using & Styling Nested Text

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,58 @@
-import { Text } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+import Title from "../components/common/Title";
+import CustomButton from "../components/common/CustomButton";
+import Colors from "../theme/colors";
 
 const GameOverScreen = () => {
-  return <Text>Game Over!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <CustomButton>Start New Game</CustomButton>
+    </View>
+  );
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
+  },
+});
